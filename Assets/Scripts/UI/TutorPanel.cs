7e439e1d42a8b4292286e9e0af7fa2121c1db4d4
Ariestellar <ahdrei47@gmail.com>
2020-06-02 18:59:03 +0300

    AddTutorPanel
    -add class TutorPanel

---
+++ Assets/Scripts/UI/TutorPanel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorPanel : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement _playerMovement;
+    [SerializeField] private GameObject _timerTemp;
+    [SerializeField] private int _displayTime;
+
+    private void OnEnable()
+    {
+        _playerMovement.Stop();
+        GameObject timer = Instantiate(_timerTemp);
+        timer.GetComponent<Timer>().StartTimer(_displayTime, Hide);
+    }
+
+    private void OnDisable()
+    {
+        _playerMovement.OnMove();
+    }
+
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
